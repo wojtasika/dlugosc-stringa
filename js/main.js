c350ee1b570dc314d00e8b365f943bb89e931859
Agnieszka Wojtasik <wojtasikagnieszka2626@gmail.com>
2018-10-19 21:47:24 +0200

    poprawa

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
+
 let text1 = 'Uwielbiam Java Script';
 let text2 = 'Jestem świetnym programistą';
 
-
-console.log(text1.length);
-console.log(text2.length);
-
 function longerText(text1, text2) {
-    if (text1 > text2) {
-        console.log(text2)
+    
+    if (text1.length > text2.length) {
+        console.log(text1)
 } else {
-    console.log(text1)
+    console.log(text2)
 }
 }
 longerText(text1, text2);
